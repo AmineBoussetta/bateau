0ffcf10827535cd446fa93fcb1d7190a34d94998
MiledAmer <midou.slm8@gmail.com>
2024-06-03 19:41:36 +0100

    adding the refs for the header

--- my-app/src/components/component/landing-page-header.tsx
+++ my-app/src/components/component/landing-page-header.tsx
@@ -12,65 +12,19 @@ export default function LandingPageHeader() {
         <a className="hover:text-gray-600 dark:hover:text-gray-400" href="#">
           Home
         </a>
-        <a className="hover:text-gray-600 dark:hover:text-gray-400" href="#">
+        <a className="hover:text-gray-600 dark:hover:text-gray-400" href="#boats">
           Boats
         </a>
-        <a className="hover:text-gray-600 dark:hover:text-gray-400" href="#">
+        <a className="hover:text-gray-600 dark:hover:text-gray-400" href="#benefits">
           Benefits
         </a>
-        <a className="hover:text-gray-600 dark:hover:text-gray-400" href="#">
+        <a className="hover:text-gray-600 dark:hover:text-gray-400" href="#contact">
           Contact
         </a>
       </nav>
       <div className="flex items-center space-x-4">
         <ThemeSwitch />
-        {/* <button className="md:hidden text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100">
-            <MenuIcon className="h-6 w-6" />
-          </button>
-          <Toggle aria-label="Toggle dark mode">
-            <MoonIcon className="h-6 w-6" />
-          </Toggle> */}
       </div>
     </header>
-  );
-}
-
-function MenuIcon(props:String) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
-    </svg>
-  );
-}
-
-function MoonIcon(props:String) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
-    </svg>
   );
 }
