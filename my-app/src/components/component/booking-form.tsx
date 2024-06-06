b281dea0a61071c7117853d0eed10c5002647c21
MiledAmer <midou.slm8@gmail.com>
2024-06-06 09:44:54 +0100

    fixing rent now button

--- my-app/src/components/component/booking-form.tsx
+++ my-app/src/components/component/booking-form.tsx
@@ -29,14 +29,7 @@ export function BookingForm(boat: BookingFormProps) {
     <Dialog defaultOpen={false}>
       <DialogTrigger asChild>
         <Button
-          className="inline-flex h-10 items-center justify-center rounded-md
-        bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow
-        transition-colors hover:bg-gray-900/90 focus-visible:outline-none
-        focus-visible:ring-1 focus-visible:ring-gray-950 hover:text-white
-        disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50
-        dark:text-gray-900 dark:hover:bg-gray-50/90
-        dark:focus-visible:ring-gray-300"
-          variant="outline"
+          className="w-full" type="submit" variant="default"
         >
           Rent Now
         </Button>
